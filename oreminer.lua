ccb7ac61a356153c1140ca2cff7c95e5a1904326
toxic8acon <toxic8acon@yahoo.com>
2013-04-26 10:36:45 -0400

    t

--- oreminer.lua
+++ oreminer.lua
@@ -10,6 +10,9 @@ end
 print("OreMiner") -- jusdt gives us some space to work with
 
 colors = {white=1,orange=2,magenta=4,lightblue=8,yellow=16,lime=32,pink=64,gray=128,lightgray=256,cyan=512,purple=1024,blue=2048,brown=4096,green=8192,red=16384, black=32768}
+rsbInputs = {read_done=1,sys_test=2,sys_on=4,s5_supplies=8,s3_cargo2=16,lime=32,pink=64,gray=128,lightgray=256,cyan=512,departure=1024,s2_cargo1=2048,s4_rails=4096,arrival=8192,s1_carttype=16384, black=32768}
+rsbOutputs = {rs_general=1,send_derailer=2,sys_test=4,unload=8,sw_cartpark=16,lime=32,sys_fault=64,gray=128,lightgray=256,rs_fuel=512,send_miner=1024,rs_rails=2048,sensor_reset=4096,sys_running=8192,sw_mine=16384, sys_stop=32768}
+
 __rsbSideIn = "left"
 __rsbSideOut = "right"
 -- test = redstone.getBundledInput("left")
@@ -18,9 +21,9 @@ function checkOutputs()
 	print("Running Output loop test")
 	for color,code in pairs(colors) do
 		redstone.setBundledOutput(__rsbSideOut, 65535)
-		sleep(2)
+		sleep(1)
 		redstone.setBundledOutput(__rsbSideOut, 0)
-		sleep(2)
+		sleep(1)
 	end
 	print("End output test")
 end
@@ -29,7 +32,7 @@ function checkInputs()
 	print("Running Input check")
 	test = redstone.getBundledInput(__rsbSideIn)
 	print("code=", test)
-	for color,code in pairs(colors) do
+	for color,code in pairs(rsbInputs) do
 		if colorTest(test, code) then
 			print(color," is ON")
 		end	
