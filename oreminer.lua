2aa7fb9efdfd9896bbec843d9f2a54da1adcd06b
toxic8acon <toxic8acon@yahoo.com>
2013-04-26 00:10:57 -0400

    test

--- oreminer.lua
+++ oreminer.lua
@@ -1,42 +1,19 @@
--- anything after two dashes is a comment, and wont be compiled.
+-- OreMiner
 --[[ this is also a comment
 and so is this
 ]]--
 
---this tutorial will teach you the basics of the print() and read() functions, if statements, as well as some variable use and math operations.
 
-
-print("Do you want to add, subtract, multiply or divide?") -- this code displays the question within the quotation marks
 op = read() -- this code creates a variable called 'op', the read() function stalls the program to accept user input, which is stored in op
-print("") -- just gives us some space to work with
+print("OreMiner") -- just gives us some space to work with
 
-print("what is the first number to be operated on?")
+num1 = 1;
+colors = {white=1,orange=2,magenta=4,lightblue=8,yellow=16,lime=32,pink=64,gray=128,lightgray=256,cyan=512,purple=1024,blue=2048,brown=4096,green=8192,red=16384, black=32768}
 
-num1 = tonumber(read()) -- accepts the first number to be stored in the variable 'number1'
-print("")
-
-print("and the second number?")
-num2 = tonumber(read())
-print("")
-
--- now we operate on the number depending on what the user typed first
-
-if op == "add" then -- if you need help with conditional statements, see this page: [1]
- result = num1+num2
-print(result) -- this prints the result
-end
-
-if op == "multiply" then
- result = num1*num2 -- asterisk represents multiply
-print(result)
-end
-
-if op == "divide" then
- result = num1/num2
-print(result)
-end
-
-if op == "subtract" then
- result = num1-num2
-print(result)
+while num1 ~= 0 do
+	for color,code in pairs(colors) do
+		print(color, (colors.test (redstone.getBundledInput("left"), code)))
+	end	
+   num1 = tonumber(read())
+   print("Press a key, 0 to exit")
 end
