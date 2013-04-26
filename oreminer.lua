8e93adba460b373a3750486e368dd429f7183d03
toxic8acon <toxic8acon@yahoo.com>
2013-04-26 09:42:35 -0400

    t

--- oreminer.lua
+++ oreminer.lua
@@ -9,24 +9,40 @@ end
 -- op = read() -- this code creates a variable called 'op', the read() function stalls the program to accept user input, which is stored in op
 print("OreMiner") -- jusdt gives us some space to work with
 
-for k,v in pairs(redstone.getSides()) do
- print(k,v)
-end
-
-num1 = 1;
 colors = {white=1,orange=2,magenta=4,lightblue=8,yellow=16,lime=32,pink=64,gray=128,lightgray=256,cyan=512,purple=1024,blue=2048,brown=4096,green=8192,red=16384, black=32768}
 -- test = redstone.getBundledInput("left")
-while num1 ~= 0 do
+
+function checkOutputs()
+	for color,code in pairs(colors) do
+		redstone.setBundledOutput("right", -1)
+		sleep(.5)
+		redstone.setBundledOutput("right", 0)
+	end
+end
+
+function checkInputs()
 	test = redstone.getBundledInput("left")
 	for color,code in pairs(colors) do
 		if colorTest(test, code) then
 			print(color," is ON")
 		end	
---		print(color, code)
---      print(test)
---		print(color, (colors.test (redstone.getBundledInput("left"), code)))
 	end	
-   num1 = tonumber(read())
-   print("Press a key, 0 to exit")
+end
+
+clear()
+while true do
+	print("1 - Run Output Test")
+	print("2 - Check Inputs Test")
+    event, param1, param2 = os.pullEvent()
+	
+	if param2 == "1" then
+	    checkOutputs()
+	end
+	if param2 == "2" then
+	    checkInputs()
+	end
+	if param2 == "e" then
+	    break
+	end
 end
 
