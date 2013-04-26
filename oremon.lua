91f8def2edb987abdd9c980fd1160cf964e24a37
toxic8acon <toxic8acon@yahoo.com>
2013-04-26 18:30:28 -0400

    t

--- oremon.lua
+++ oremon.lua
@@ -70,7 +70,7 @@ while true do
 			print(param2)
 			local stat = setCartStatus
 			monitor.write(param2)
-			for k, v in string.gmatch(param2, ":(%w+)") do
+			for k, v in string.gmatch(param2, "(%w+):") do
    				print(v)
 			end
 		end
