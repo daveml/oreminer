50ad2027fe7c80e9dffad66362c9debed9c945d2
toxic8acon <toxic8acon@yahoo.com>
2013-04-26 18:01:46 -0400

    t

--- oreminer.lua
+++ oreminer.lua
@@ -13,17 +13,19 @@ __rsbSideIn = "left"
 __rsbSideOut = "right"
 __miner = "Miner"
 __derailer = "Derailer"
+__false = 0
+__true = 1
 
 colors = {white=1,orange=2,magenta=4,lightblue=8,yellow=16,lime=32,pink=64,gray=128,lightgray=256,cyan=512,purple=1024,blue=2048,brown=4096,green=8192,red=16384, black=32768}
 rsbInputs = {read_done=1,sys_test=2,sys_on=4,s5_torches=8,s3_cargo2=16,lime=32,s6_bridge=64,gray=128,lightgray=256,cyan=512,departure=1024,s2_cargo1=2048,s4_rails=4096,arrival=8192,s1_carttype=16384, black=32768}
 rsbOutputs = {rs_general=1,send_derailer=2,sys_test=4,unload=8,sw_cartpark=16,lime=32,sys_fault=64,gray=128,lightgray=256,rs_fuel=512,send_miner=1024,rs_rails=2048,sensor_reset=4096,sys_running=8192,sw_mine=16384, sys_stop=32768}
 
 sensorCartType = {__miner,__derailer}
-sensorCargoFull = {false,true}
-sensorCargoEmpty = {false, true}
-sensorHasRails = {false,true}
-sensorHasTorches = {false,true}
-sensorHasBridge = {false,true}
+sensorCargoFull = {__false,__true}
+sensorCargoEmpty = {__false, __true}
+sensorHasRails = {__false,__true}
+sensorHasTorches = {__false,__true}
+sensorHasBridge = {__false,__true}
 
 Cart = { [sensorCartType]="cartType", [sensorCargoFull]="cargoFull", [sensorCargoEmpty]="cargoEmpty", [sensorHasRails]="hasRails", [sensorHasTorches]="hasTorches", [sensorHasBridge]="hasBridge" }
 
@@ -52,20 +54,20 @@ end
 
 function readCart()
 	Cart.cartType = __miner
-	Cart.cargoFull = true
-	Cart.cargoEmpty = true
-	Cart.hasRails = true
-	Cart.hasTorches = true
-	Cart.hasBridge = true
+	Cart.cargoFull = __true
+	Cart.cargoEmpty = __true
+	Cart.hasRails = __true
+	Cart.hasTorches = __true
+	Cart.hasBridge = __true
 end
 
 function resetCart()
 	Cart.cartType = __derailer
-	Cart.cargoFull = false
-	Cart.cargoEmpty = false
-	Cart.hasRails = false
-	Cart.hasTorches = false
-	Cart.hasBridge = false
+	Cart.cargoFull = __false
+	Cart.cargoEmpty = __false
+	Cart.hasRails = __false
+	Cart.hasTorches = __false
+	Cart.hasBridge = __false
 end
 
 function Init()	
@@ -134,19 +136,19 @@ while true do
 			Cart.cartType = __miner
 		end
 		if colorTest(input, rsbInputs.s2_cargo1) then
-			Cart.cargoEmpty = true
+			Cart.cargoEmpty = __true
 		end
 		if colorTest(input, rsbInputs.s3_cargo2) then
-			Cart.cargoFull = true
+			Cart.cargoFull = __true
 		end
 		if colorTest(input, rsbInputs.s4_rails) then
-			Cart.hasRails = true
+			Cart.hasRails = __true
 		end
 		if colorTest(input, rsbInputs.s5_torches) then
-			Cart.hasTorches = true
+			Cart.hasTorches = __true
 		end
 		if colorTest(input, rsbInputs.s6_bridge) then
-			Cart.hasBridge = true
+			Cart.hasBridge = __true
 		end
 		if colorTest(input, rsbInputs.read_done) then
 			print("Cart scan complete!\n")
