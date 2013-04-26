97e6c5baeea2450ec50033f0a5c4b9b7213f0ed3
toxic8acon <toxic8acon@yahoo.com>
2013-04-26 00:36:54 -0400

    tst

--- oreminer.lua
+++ oreminer.lua
@@ -7,6 +7,10 @@ and so is this
 op = read() -- this code creates a variable called 'op', the read() function stalls the program to accept user input, which is stored in op
 print("OreMiner") -- just gives us some space to work with
 
+for k,v in pairs(redstone.getSides()) do
+ print(k,v)
+end
+
 num1 = 1;
 colors = {white=1,orange=2,magenta=4,lightblue=8,yellow=16,lime=32,pink=64,gray=128,lightgray=256,cyan=512,purple=1024,blue=2048,brown=4096,green=8192,red=16384, black=32768}
 print (colors.test (redstone.getBundledInput("back"), colors.blue))
