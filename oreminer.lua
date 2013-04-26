6c5bfbfc694d43db90dfde5c79473f4cba557432
toxic8acon <toxic8acon@yahoo.com>
2013-04-26 00:51:18 -0400

    test

--- oreminer.lua
+++ oreminer.lua
@@ -4,7 +4,7 @@ and so is this
 ]]--
 
 
-op = read() -- this code creates a variable called 'op', the read() function stalls the program to accept user input, which is stored in op
+-- op = read() -- this code creates a variable called 'op', the read() function stalls the program to accept user input, which is stored in op
 print("OreMiner") -- just gives us some space to work with
 
 for k,v in pairs(redstone.getSides()) do
@@ -13,11 +13,13 @@ end
 
 num1 = 1;
 colors = {white=1,orange=2,magenta=4,lightblue=8,yellow=16,lime=32,pink=64,gray=128,lightgray=256,cyan=512,purple=1024,blue=2048,brown=4096,green=8192,red=16384, black=32768}
-print (colors.test (redstone.getBundledInput("back"), colors.blue))
+-- test = redstone.getBundledInput("left")
 while num1 ~= 0 do
 	for color,code in pairs(colors) do
 		print(color, code)
-		print(color, (colors.test (redstone.getBundledInput("left"), code)))
+		test = redstone.getBundledInput("left")
+		print(test)
+--		print(color, (colors.test (redstone.getBundledInput("left"), code)))
 	end	
    num1 = tonumber(read())
    print("Press a key, 0 to exit")
